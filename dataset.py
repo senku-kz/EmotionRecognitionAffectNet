cb08929abec1650559b19579e7819ffe91ae0afc
senku <senku.kz@gmail.com>
2022-03-27 02:04:04 +0600

    add try-except

--- dataset.py
+++ dataset.py
@@ -31,17 +31,21 @@ def sort_train_images():
             filename = entry.split('.')[0]
             filename_annotation = os.path.join(folder_annotations, f'{filename}_exp.npy')
             if os.path.exists(filename_annotation):
-                # data_array = np.load(filename_annotation)
-                data_array = np.load(filename_annotation, allow_pickle=True)
-                emotion_idx = data_array.item()
-                if not os.path.exists(os.path.join(folder_dst, emotions[emotion_idx])):
-                    os.makedirs(os.path.join(folder_dst, emotions[emotion_idx]))
-                shutil.copyfile(
-                    os.path.join(folder_src, entry),
-                    os.path.join(folder_dst, emotions[emotion_idx], entry)
-                )
-                insert_filenames(dataset='train', filename=entry, class_label=emotions[emotion_idx])
-                cnt += 1
+                try:
+                    # data_array = np.load(filename_annotation)
+                    data_array = np.load(filename_annotation, allow_pickle=True)
+                    emotion_idx = data_array.item()
+                    if not os.path.exists(os.path.join(folder_dst, emotions[emotion_idx])):
+                        os.makedirs(os.path.join(folder_dst, emotions[emotion_idx]))
+                    shutil.copyfile(
+                        os.path.join(folder_src, entry),
+                        os.path.join(folder_dst, emotions[emotion_idx], entry)
+                    )
+                    insert_filenames(dataset='train', filename=entry, class_label=emotions[emotion_idx])
+                    cnt += 1
+                except:
+                    print("Error file", filename_annotation)
+
     print(f'Copy {cnt} files')
 
 
@@ -74,16 +78,19 @@ def sort_validation_images():
             filename_annotation = os.path.join(folder_annotations, f'{filename}_exp.npy')
             if os.path.exists(filename_annotation):
                 # data_array = np.load(filename_annotation)
-                data_array = np.load(filename_annotation, allow_pickle=True)
-                emotion_idx = data_array.item()
-                if not os.path.exists(os.path.join(folder_dst, emotions[emotion_idx])):
-                    os.makedirs(os.path.join(folder_dst, emotions[emotion_idx]))
-                shutil.copyfile(
-                    os.path.join(folder_src, entry),
-                    os.path.join(folder_dst, emotions[emotion_idx], entry)
-                )
-                insert_filenames(dataset='validation', filename=entry, class_label=emotions[emotion_idx])
-                cnt += 1
+                try:
+                    data_array = np.load(filename_annotation, allow_pickle=True)
+                    emotion_idx = data_array.item()
+                    if not os.path.exists(os.path.join(folder_dst, emotions[emotion_idx])):
+                        os.makedirs(os.path.join(folder_dst, emotions[emotion_idx]))
+                    shutil.copyfile(
+                        os.path.join(folder_src, entry),
+                        os.path.join(folder_dst, emotions[emotion_idx], entry)
+                    )
+                    insert_filenames(dataset='validation', filename=entry, class_label=emotions[emotion_idx])
+                    cnt += 1
+                except:
+                    print("Error file", filename_annotation)
     print(f'Copy {cnt} files')
 
 
