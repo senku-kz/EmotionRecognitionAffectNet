d0dd977c9ff35d58e72108d631b2c75d167d82a2
senku <senku.kz@gmail.com>
2022-03-27 23:28:16 +0600

    swich for two datasets

--- parameters.py
+++ parameters.py
@@ -1,12 +1,12 @@
 
-# db_sql_file = '../dataset/sqlite3.db'
-# dataset_lst = ['train_set', 'val_set']
-# folder_dst_lst = ['../dataset/train', '../dataset/validation']
-# # folder_dst_dic = {k: v for k, v in zip(dataset_lst, folder_dst_lst)}
-# folder_dst_dic = {i.split('/')[-1]: i for i in folder_dst_lst}
-
 db_sql_file = '../dataset/sqlite3.db'
-dataset_lst = ['val_set']
-folder_dst_lst = ['../dataset/validation']
+dataset_lst = ['train_set', 'val_set']
+folder_dst_lst = ['../dataset/train', '../dataset/validation']
+# folder_dst_dic = {k: v for k, v in zip(dataset_lst, folder_dst_lst)}
 folder_dst_dic = {i.split('/')[-1]: i for i in folder_dst_lst}
+
+# db_sql_file = '../dataset/sqlite3.db'
+# dataset_lst = ['val_set']
+# folder_dst_lst = ['../dataset/validation']
+# folder_dst_dic = {i.split('/')[-1]: i for i in folder_dst_lst}
 
