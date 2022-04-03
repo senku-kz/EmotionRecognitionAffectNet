a03b530e097e4d250908843450be3c5d58198815
senku <senku.kz@gmail.com>
2022-04-03 14:17:30 +0600

    add ResNet model to test process

--- models_test.py
+++ models_test.py
@@ -1,8 +1,11 @@
 from discrete_categories import camera_positions
-from models.CoAtNet import coatnet_0
 from models_train import test_the_model, test_model_separate_accuracy
 from parameters import model_dst
 from split_dataset import ds_test_cam
+
+from models.VGG import VGG
+from models.CoAtNet import coatnet_0
+from models.ResNet import ResNet50
 
 
 def test_coatnet():
@@ -11,7 +14,21 @@ def test_coatnet():
     v_dataset_test, v_dataloader_test = ds_test_cam(camera)
     v_classes = v_dataset_test.classes
     v_model = coatnet_0(num_classes=len(v_classes))
-    test_the_model(v_model, v_dataset_test, v_dataloader_test, criterion=None, model_dst='models_trained')
+    test_the_model(v_model, v_dataset_test, v_dataloader_test, criterion=None, model_dst=model_dst)
+
+    # for camera in camera_positions:
+    #     v_dataset_test, v_dataloader_test = ds_test_cam(camera)
+    #     if v_dataset_test and v_dataloader_test:
+    #         test_model_separate_accuracy(v_model, v_dataset_test, v_dataloader_test, criterion=None, model_dst=model_dst, camera=camera)
+
+
+def test_resnet():
+    # Test model
+    camera = 'all'
+    v_dataset_test, v_dataloader_test = ds_test_cam(camera)
+    v_classes = v_dataset_test.classes
+    v_model = ResNet50(num_classes=len(v_classes))
+    test_the_model(v_model, v_dataset_test, v_dataloader_test, criterion=None, model_dst=model_dst)
 
     # for camera in camera_positions:
     #     v_dataset_test, v_dataloader_test = ds_test_cam(camera)
@@ -20,5 +37,6 @@ def test_coatnet():
 
 
 if __name__ == '__main__':
-    test_coatnet()
+    test_resnet()
+    # test_coatnet()
     pass
