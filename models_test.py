da103502aea4254b891d77c35772736defd18215
senku <senku.kz@gmail.com>
2022-04-04 18:16:46 +0600

    test models file

--- models_test.py
+++ models_test.py
@@ -10,25 +10,33 @@ from models.CoAtNet import coatnet_0
 from models.ResNet import ResNet50
 
 
-def test_coatnet():
-    # Test model
-    v_dataset_test, v_dataloader_test = ds_test_all()
-    v_classes = v_dataset_test.classes
-    v_model = coatnet_0(num_classes=len(v_classes))
-    test_the_model(v_model, v_dataset_test, v_dataloader_test, criterion=None, model_dst=model_dst)
+# def test_coatnet():
+#     # Test model
+#     v_dataset_test, v_dataloader_test = ds_test_all()
+#     v_classes = v_dataset_test.classes
+#     v_model = coatnet_0(num_classes=len(v_classes))
+#     test_the_model(v_model, v_dataset_test, v_dataloader_test, criterion=None, model_dst=model_dst)
+#
+#     for camera in camera_positions:
+#         v_dataset_test, v_dataloader_test = ds_test_cam(camera)
+#         if v_dataset_test and v_dataloader_test:
+#             test_model_separate_accuracy(v_model, v_dataset_test, v_dataloader_test, criterion=None, model_dst=model_dst, camera=camera)
 
-    for camera in camera_positions:
-        v_dataset_test, v_dataloader_test = ds_test_cam(camera)
-        if v_dataset_test and v_dataloader_test:
-            test_model_separate_accuracy(v_model, v_dataset_test, v_dataloader_test, criterion=None, model_dst=model_dst, camera=camera)
 
-
-def test_resnet():
+def test_model(model='ResNet'):
     # Test model
     v_dataset_test, v_dataloader_test = ds_test_all()
     logging.info(f'Number of test examples: \t {len(v_dataset_test)}')
     v_classes = v_dataset_test.classes
-    v_model = ResNet50(num_classes=len(v_classes))
+
+    if model == 'CoAtNet':
+        v_model = coatnet_0(num_classes=len(v_classes))
+    elif model == 'ResNet':
+        v_model = ResNet50(img_channel=3, num_classes=len(v_classes))
+    elif model == 'VGG':
+        v_model = VGG(in_channels=3, num_classes=len(v_classes))
+
+    # v_model = ResNet50(num_classes=len(v_classes))
     test_the_model(v_model, v_dataset_test, v_dataloader_test, criterion=None, model_dst=model_dst)
     test_model_separate_accuracy(v_model, v_dataset_test, v_dataloader_test, criterion=None, model_dst=model_dst)
 
@@ -41,6 +49,5 @@ def test_resnet():
 
 
 if __name__ == '__main__':
-    test_resnet()
-    # test_coatnet()
+    test_model('ResNet')
     pass
