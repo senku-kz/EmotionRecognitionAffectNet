5835ce11e486761a9d6bb5cd7eda89d7aa80cdbe
senku <senku.kz@gmail.com>
2022-04-05 09:19:38 +0600

    test vgg model

--- models_test.py
+++ models_test.py
@@ -10,19 +10,6 @@ from models.CoAtNet import coatnet_0
 from models.ResNet import ResNet50
 
 
-# def test_coatnet():
-#     # Test model
-#     v_dataset_test, v_dataloader_test = ds_test_all()
-#     v_classes = v_dataset_test.classes
-#     v_model = coatnet_0(num_classes=len(v_classes))
-#     test_the_model(v_model, v_dataset_test, v_dataloader_test, criterion=None, model_dst=model_dst)
-#
-#     for camera in camera_positions:
-#         v_dataset_test, v_dataloader_test = ds_test_cam(camera)
-#         if v_dataset_test and v_dataloader_test:
-#             test_model_separate_accuracy(v_model, v_dataset_test, v_dataloader_test, criterion=None, model_dst=model_dst, camera=camera)
-
-
 def test_model(model='ResNet'):
     # Test model
     v_dataset_test, v_dataloader_test = ds_test_all()
