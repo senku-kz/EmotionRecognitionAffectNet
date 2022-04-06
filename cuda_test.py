556bcf21c74f004fbc0cf83bd7d3bf961331eb30
senku <senku.kz@gmail.com>
2022-04-06 12:49:10 +0600

    print cuda version

--- cuda_test.py
+++ cuda_test.py
@@ -9,6 +9,7 @@ pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio===0.11.0+cu
 if __name__ == '__main__':
     # Step 1: Check Pytorch (optional)
     import torch
+    print("Cuda version: ", torch.__version__)
     print("Cuda available: ", torch.cuda.is_available())
     print("Device name:", torch.cuda.get_device_name())
 
