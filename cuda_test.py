1edfa5cad4c4f6ece71be5e71c92e427ac32ed92
senku <senku.kz@gmail.com>
2022-04-06 13:25:13 +0600

    add GPU memory info

--- cuda_test.py
+++ cuda_test.py
@@ -14,6 +14,12 @@ if __name__ == '__main__':
     print("Cuda device: ", torch.cuda.current_device())
     print("Device name:", torch.cuda.get_device_name())
 
+    t = torch.cuda.get_device_properties(0).total_memory
+    r = torch.cuda.memory_reserved(0)
+    a = torch.cuda.memory_allocated(0)
+    f = r - a  # free inside reserved
+    print('GPU memory:', t, r, a, f)
+
     # Step 2: Check Tensorflow
     # from tensorflow.python.client import device_lib
     # print('-'*60)
