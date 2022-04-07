87ec1ba67af83149b8c9da2d66be22bfdfde0d53
senku <senku.kz@gmail.com>
2022-04-07 10:05:26 +0600

    add model file testing

--- cuda_test.py
+++ cuda_test.py
@@ -1,5 +1,7 @@
-from db_sqlAlchemy import HeadPositionTrain, HeadPositionValidation, session
+import os.path
 
+from db_sqlAlchemy import HeadPositionTrain, HeadPositionValidation, session
+from parameters import model_dst
 
 """
 pip install tensorflow
@@ -8,6 +10,7 @@ pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio===0.11.0+cu
 """
 if __name__ == '__main__':
     # Step 1: Check Pytorch (optional)
+    print('='*20, 'Test Torch - Cuda library', '='*20)
     import torch
     print("Cuda version: ", torch.__version__)
     print("Cuda available: ", torch.cuda.is_available())
@@ -30,6 +33,17 @@ if __name__ == '__main__':
     # print(K.tensorflow_backend._get_available_gpus())
 
     # Step 4: Test DB count
-    print('Test SQLite file:')
+    print('='*20, 'Test SQLite file', '='*20)
     print(f'Row count in {HeadPositionTrain.__tablename__} is {session.query(HeadPositionTrain).count()}')
     print(f'Row count in {HeadPositionValidation.__tablename__} is {session.query(HeadPositionValidation).count()}')
+
+    # Step 5: Test model files
+    print('='*20, 'Test model files', '='*20)
+    files = ['VGG_tmp.pt', 'VGG_final.pt'
+             , 'ResNet_tmp.pt', 'ResNet_final.pt'
+             , 'CoAtNet_tmp.pt', 'CoAtNet_final.pt']
+    for filename in files:
+        url = os.path.join(model_dst, filename)
+        if os.path.exists(url):
+            loaded_file = torch.load(url)
+            print(filename, loaded_file['epochs'])
